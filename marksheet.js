bd089e3dbf027ae95b48c7249b05acdb03091948
Rashmikaa21 <nmrrashmi@gmail.com>
2026-09-21 23:13:25 +0800

    first commit

---
+++ marksheet.js
@@ -0,0 +1,53 @@
+// Student Marksheet 
+
+let studentName = "Rashmikaa";
+console.log("Rashmikaa's 2026 Exam Results");
+
+let Marks = 90;
+let Internals = 5;
+console.log("Marks = ",Marks +=Internals);
+
+let Grade;
+let Result = (Marks >= 50 ?   "Pass" : "Fail");
+console.log("Results = ",Result);
+
+if(Marks >= 90)
+{
+    Grade = "A";
+}
+ 
+else if(Marks >= 70)
+{
+    Grade = "B";
+}
+
+else if(Marks >= 50)
+{
+   Grade = "C";
+}
+
+else
+{
+    Grade = "F";
+}
+
+console.log("Grade = ",Grade)
+
+switch(Grade)
+{
+    case "A" :
+        console.log("Remarks = Execellent");
+    break
+
+    case "B" :
+        console.log("Remarks = Good");
+    break
+
+    case "C" :
+        console.log("Remarks = Average");
+    break
+
+    default :
+        console.log("Remarks = Needs Improvement");
+}
+
---
+++ script.js
@@ -0,0 +1,78 @@
+// Calculator using if else
+console.log("Calculator Result");
+
+let a = 10;
+let b = 0;
+
+let operator = "/";
+
+if (operator === "+")
+
+{console.log(a+b);}
+
+else if(operator === "-")
+
+    {console.log(a-b);}
+
+    else if (operator === "*")
+
+        {console.log(a*b);}
+
+        else if (operator === "/")
+        {
+            if (b != 0)
+           {console.log(a/b);}
+            else
+               { console.log("Denominator cannot be zero");
+            }
+        }
+
+            else
+            {console.log("Invalid operator used");}
+
+
+
+// Calculator using Switch
+console.log("Calculator Result");
+
+let c = 100;
+let d = 10;
+
+let operator1 = "%";
+
+switch(operator1)
+{
+    case "+":
+        console.log(c+d);
+    break;
+
+    case "-":
+        console.log(c-d);
+     break;
+
+     case "*":
+        console.log(c*d);
+     break;
+
+    case "/":
+         if (d != 0)
+           {console.log(c/d);}
+            else
+               { console.log("Denominator cannot be zero");
+            }
+     break;
+
+    default:
+        console.log("Invalid operator used");
+    
+}
+
+
+
+
+
+
+
+
+
+
